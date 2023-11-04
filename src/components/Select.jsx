dd71ba40dbbcc64005fe9df7bf25cea9773d0285
ziad-elshihy <elshihyziad@gmail.com>
2023-11-04 21:13:21 +0200

    ...

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,34 +1,41 @@
 /* eslint-disable react/prop-types */
+import { RiArrowDownSFill } from 'react-icons/ri'
 
 const Select = ({ handleData, selectValue, setSelectValue }) => {
    return (
-      <select
-         onClick={handleData}
-         value={selectValue}
-         onChange={(e) => setSelectValue(e.target.value)}
-      >
-         <option value='empty'>اختر</option>
-         <option
-            value='المزارع'
+      <div className='select'>
+         <select
+            onClick={handleData}
+            value={selectValue}
+            onChange={(e) => setSelectValue(e.target.value)}
          >
-            المزارع
-         </option>
-         <option
-            value='مربي الحيوانات'
-         >
-            مربي الحيوانات
-         </option>
-         <option
-            value='سائق'
-         >
-            سائق
-         </option>
-         <option
-            value='طالب'
-         >
-            طالب
-         </option>
-      </select>
+            <option value='empty'>اختر</option>
+            <option
+               value='المزارع'
+            >
+               المزارع
+            </option>
+            <option
+               value='مربي الحيوانات'
+            >
+               مربي الحيوانات
+            </option>
+            <option
+               value='سائق'
+            >
+               سائق
+            </option>
+            <option
+               value='طالب'
+            >
+               طالب
+            </option>
+         </select>
+         <RiArrowDownSFill
+            size={22}
+            className='icon'
+         />
+      </div>
    )
 }
 
